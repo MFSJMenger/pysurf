ab9a631291abdd85860b3fc6ba6a2bf0b76eaf73
Maximilian Menger <menger.maximilian@gmail.com>
2019-09-20 12:27:00 +0200

    added landau zener

--- pysurf/sh/sh.py
+++ pysurf/sh/sh.py
@@ -11,6 +11,41 @@ def vv_vstep(v, a_old, a, dt):
     return v
 
 
-def surface_hopping_step(crd, v, a):
-    pass
+def abs_gt(a, b):
+    return abs(a) > abs(b)
+
+def compute_landau_zener_probability(dt, d_curr, 
+        d_prev_step, d_two_step_prev):
+    finite_difference_grad = (d_curr + d_two_step_prev - 2 * d_prev_step) / dt**2        
+class LandauZener(object):
+
+    @staticmethod
+    def compute_diff(e, i, j):
+        """return e[i] - e[j]"""
+        return e[i] - e[j]
+
+    @staticmethod
+    def landau_zener_select(iactive, nstates, dt, 
+            e_curr, e_prev_step, e_two_step_prev):
+        """"takes in the (adiabatic?) energies at
+            the current, the previous, and the two steps previous time step
+
+            select which state is the most likely to hop to
+        """
+
+        for istate in range(nstates):
+            # 
+            if istate == iactive:
+                continue
+            # select state
+            d_two_step_prev = compute_diff(e_two_step_prev, iactive, istate)
+            d_prev_step = compute_diff(e_prev_step, iactive, istate)
+            d_curr = compute_diff(e_curr, iactive, istate)
+            if (abs_gt(d_curr, d_prev_step) and abs_gt(d_two_step_prev, d_prev_step)):
+                chop = compute_landau_zener_probability(dt, d_curr, 
+                        d_prev_step, d_two_step_prev)
+
+
+
+
 
---
+++ pysurf/utils/osutils.py
@@ -0,0 +1,16 @@
+import os
+
+
+def exists_and_isfile(filename):
+    """if file does not exist, create it
+       if file exisits check if isfile or  raise Exception
+    """
+
+    if os.path.exists(filename):
+        if os.path.isfile(filename):
+            return True
+        else:
+            raise Exception("Object '%s' exisits but is not a file!")
+
+    return False
+        
