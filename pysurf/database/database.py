1a077119ae05a72ee3ca3aa62f822a18509a3a46
Maximilian Menger <menger.maximilian@gmail.com>
2019-10-01 14:07:06 +0200

    updated database class

--- pysurf/database/database.py
+++ pysurf/database/database.py
@@ -18,7 +18,7 @@ class Database(object):
     
     """
 
-    __slots__ = ('filename', '_rep', '_db', '_handle', 'closed')
+    __slots__ = ('filename', '_rep', '_db', '_handle', 'closed', '_icurrent')
 
     def __init__(self, filename, settings):
         """Initialize new Database,
@@ -37,21 +37,40 @@ class Database(object):
         self._db, self._handle = self._rep.create_database(filename)
         # 
         self.closed = False
+        # 
+        self._icurrent = None
 
-    def __getattr__(self, key):
-        if key in self._handle:
-            return self._handle[key]
-        return object.__getattribute__(self, key)
+
+    def __getitem__(self, key):
+        return self._handle.get(key, None)
+
+    def __contains__(self, key):
+        return key in self._handle
+
+    def get(self, key, ivalue):
+        variable = self._handle[key]
+        if variable.shape[0] > ivalue:
+            return variable[ivalue]
+
+    def set(self, ivalue, key, value):
+        if key in self:
+            self._handle[key][ivalue, :] = value
 
     def get_dimension_size(self, key):
         if key in self._db.dimensions.keys():
             return self._db.dimensions[key].size
 
+    @property
+    def increase(self):
+        self._icurrent += 1
+
     def append(self, key, value):
         variable = self._handle[key]
         unlimited = variable.get_dims()[0]
         assert(unlimited.isunlimited())
-        variable[unlimited.size, :] = value
+        if self._icurrent is None:
+            self._icurrent = unlimited.size
+        variable[self._icurrent, :] = value
 
     def close(self):
         if self.closed is False:
