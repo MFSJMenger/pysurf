a81657afdbc60be23726be503ad90fb0bb04cba8
Maximilian Menger <menger.maximilian@gmail.com>
2020-04-17 19:28:39 +0200

    updated setup

--- bin/setup_propagation.py
+++ bin/setup_propagation.py
@@ -1,14 +1,16 @@
 import os
 import numpy as np
-from shutil import copy2
-
+from shutil import copy2 as copy
+#
 from pysurf.colt import Colt
 from pysurf.colt import FromCommandline
-from pysurf.utils.osutils import exists_and_isfile
+from pysurf.utils import SubfolderHandle
 from pysurf.logger import get_logger
 from pysurf.sampling import Sampling
 
+
 class SetupPropagation(Colt):
+
     _questions = """
     # Total propagation time in fs
     propagation time in fs = 100 :: float
@@ -31,44 +33,36 @@ class SetupPropagation(Colt):
     [copy_db(yes)]
     db_file = none :: str
     """
-    
+
 
     def __init__(self, inputfile):
         """ Class to create initial conditions due to user input. Initial conditions are saved 
             in a file for further usage.
         """
         self.inputfile = inputfile
-        self.propagationfolder = 'prop'
-        self.trajfolder = 'traj_'
         self.logger = get_logger('setup_propagation.log', 'setup_propagation')
-        
-        quests = self.generate_questions("SURFACE HOPPING", config=inputfile)
-        self.config = quests.ask(inputfile)
 
-        if not(os.path.isdir(self.propagationfolder)):
-            os.mkdir(self.propagationfolder)
+        fhandle = SubfolderHandle('prop', 'traj')
         
+        quests = self.generate_questions(config=inputfile)
+        config = quests.ask(inputfile)
+
+        n_traj = config['n_traj']
+
         # Open DB of initial conditions once, so that it is available
-        self.sampling = Sampling.from_db(self.config['sampling_db'])
+        self.sampling = Sampling.from_db(config['sampling_db'])
 
-        for i in range(self.config['n_traj']):
-            self._setup_trajectory_folder(i)
+        for i, folder in enumerate(fhandle.folderiter(range(n_traj))):
+            self._setup_trajectory_folder(i, folder, config)
 
-    def _setup_trajectory_folder(self, number):
-        foldername = os.path.join(self.propagationfolder, self.trajfolder + '{:04d}'.format(number))
-        if os.path.isdir(foldername):
-            self.logger.info('Folder {0} already exists. Skipping trajectory'.format(foldername))
-            return
+    def _setup_trajectory_folder(self, number, foldername, config):
+        copy(self.inputfile, foldername)
+        copy(config['spp'], foldername)
 
-        os.mkdir(foldername)
+        if config['copy_db'] == 'yes':
+            copy(config['copy_db']['db_file'], foldername)
 
-        copy2(self.inputfile, foldername)
-        copy2(self.config['spp'], foldername)
-
-        if self.config['copy_db'] == 'yes':
-            copy2(self.config['copy_db']['db_file'], foldername)
-
-        initname = os.path.join(foldername, 'init_{:04d}.db'.format(number))
+        initname = os.path.join(foldername, 'init.db')
         self.sampling.export_condition(initname, number)
 
 
@@ -77,6 +71,7 @@ inputfile = propagation.inp :: file
 """)
 def command_setup_propagation(inputfile):
     SetupPropagation(inputfile)
+
 
 if __name__=="__main__":
     command_setup_propagation()
---
+++ pysurf/setup.py
@@ -0,0 +1,34 @@
+from abc import abstractmethod
+#
+from .utils import SubfolderHandle
+from .logger import get_logger
+from .colt import Colt
+
+
+class SetupBase(Colt, SubfolderHandle):
+
+    folder = None
+    subfolder = None
+
+    def __init__(self, logger=None, digit=8):
+        if self.folder is None:
+            raise Exception("self.folder needs to be set!")
+        if self.subfolder is None:
+            raise Exception("self.subfolder needs to be set!")
+        #
+        SubfolderHandle.__init__(self, self.folder, self.subfolder)
+        #
+        if logger is None:
+            self.logger = get_logger(None, "")
+        else:
+            self.logger = logger
+
+    def setup_folders(self, lst, *args, **kwargs):
+        self.logger.info(f'Start setting up folders\n')
+        for i, folder in enumerate(self.folderiter(lst, skip_existing)):
+            self.logger.info(f'Create folder {folder}\n')
+            self.setup_folder(i, folder, *args, **kwargs)
+
+    @abstractmethod                
+    def setup_folder(self, number, foldername, *args, **kwargs):
+        """fill the folder with data"""
