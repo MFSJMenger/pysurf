4ad3e2b0fae35388339214895c847f2016de4d3d
Maximilian Menger <menger.maximilian@gmail.com>
2020-10-27 18:50:41 +0100

    updated library

--- pysurf/__init__.py
+++ pysurf/__init__.py
@@ -17,6 +17,6 @@ base = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 core_plugins = os.path.join(base, "core_plugins")
 user_plugins = os.path.join(base, "plugins")
 # load core plugins
-PluginLoader(core_plugins)
+PluginLoader(core_plugins, ignorefile='plugins.ini')
 # load user plugins
-PluginLoader(user_plugins)
+PluginLoader(user_plugins, ignorefile='plugins.ini')
